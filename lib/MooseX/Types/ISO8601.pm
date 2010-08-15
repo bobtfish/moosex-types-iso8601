3bc2221d18023500d4430d3ebf46ab397c5d678d
Tomas Doran <bobtfish@bobtfish.net>
2010-08-15 18:23:30 +0100

    Version 0.04

--- lib/MooseX/Types/ISO8601.pm
+++ lib/MooseX/Types/ISO8601.pm
@@ -7,7 +7,7 @@ use MooseX::Types::Moose qw/Str Num/;
 use List::MoreUtils qw/ zip /;
 use namespace::autoclean;
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 
 use MooseX::Types -declare => [qw(
     ISO8601DateStr
